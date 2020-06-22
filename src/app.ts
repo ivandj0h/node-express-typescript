d81b9c98cad69ce078e934dd7ba62968a84bf71c
ivandi1980 <ivandi.djoh@gmail.com>
2020-06-22 22:54:58 +0700

    added some function

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
-import express, { response } from 'express';
-import { request } from 'http';
+import express, { Application, Request, Response, NextFunction } from 'express';
 
-const app = express();
+const app: Application = express();
 
-app.get('/', (request, response) => {
-    response.send('Hello World');
+const add = (a: number, b: number): number => a ** b;
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+    res.send(`Hasil dari 2 <sup>2</sup> adalah ${add(2, 2)}`);
 });
 
 app.listen(5000, () => console.log('Server Running...'))
